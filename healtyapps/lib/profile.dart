04b1665d7f9a7a9bf7366a0ada5e621c5e1ef33b
Gustian Arya <gustianarya12@gmail.com>
2023-05-25 11:13:13 +0700

    Tambah Profile

---
+++ healtyapps/lib/profile.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+
+import 'navbar.dart';
+
+// void main() {
+//   runApp(Profile());
+// }
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            color: Colors.white54,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 15,
+                ),
+                const ListTile(
+                  leading: Icon(Icons.arrow_back),
+                  // trailing: Icon(Icons.menu),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Profile",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircleAvatar(
+                      maxRadius: 65,
+                      backgroundImage: AssetImage("images/Profile.jpg"),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: const [
+                //     CircleAvatar(
+                //       backgroundImage: AssetImage("images/download.png"),
+                //     ),
+                //     SizedBox(
+                //       width: 15,
+                //     ),
+                //     CircleAvatar(
+                //       backgroundImage:
+                //           AssetImage("images/GooglePlus-logo-red.png"),
+                //     ),
+                //     SizedBox(
+                //       width: 15,
+                //     ),
+                //     CircleAvatar(
+                //       backgroundImage: AssetImage(
+                //           "images/1_Twitter-new-icon-mobile-app.jpg"),
+                //     ),
+                //     SizedBox(
+                //       width: 15,
+                //     ),
+                //     CircleAvatar(
+                //       backgroundImage:
+                //           AssetImage("images/600px-LinkedIn_logo_initials.png"),
+                //     )
+                //   ],
+                // ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Expanded(
+                      child: ListView(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        color: Colors.white70,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          // leading: Icon(
+                          //   Icons.privacy_tip_sharp,
+                          //   color: Colors.black54,
+                          // ),
+                          title: Text(
+                            'Username',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          // leading: Icon(
+                          //   Icons.history,
+                          //   color: Colors.black54,
+                          // ),
+                          title: Text(
+                            'Nama',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          // leading:
+                          //     Icon(Icons.help_outline, color: Colors.black54),
+                          title: Text(
+                            'Email',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          // leading: Icon(
+                          //   Icons.privacy_tip_sharp,
+                          //   color: Colors.black54,
+                          // ),
+                          title: Text(
+                            'Alamat',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(Icons.arrow_forward_ios_outlined),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          // leading: Icon(
+                          //   Icons.add_reaction_sharp,
+                          //   color: Colors.black54,
+                          // ),
+                          title: Text(
+                            'No Telp',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(
+                            Icons.arrow_forward_ios_outlined,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Card(
+                        color: Colors.white70,
+                        margin: const EdgeInsets.only(
+                            left: 35, right: 35, bottom: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const ListTile(
+                          leading: Icon(
+                            Icons.logout,
+                            color: Colors.black54,
+                          ),
+                          title: Text(
+                            'Logout',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Icon(Icons.arrow_forward_ios_outlined),
+                        ),
+                      )
+                    ],
+                  )),
+                )
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomNavbar(),
+        ));
+  }
+}
